8fa1eef487f4079e618565a6718fb919a5f02194
Adrian Serafin <adrian@softmad.pl>
2019-07-04 19:15:32 +0200

    improve examples

--- common_table_expressions.sql
+++ common_table_expressions.sql
@@ -1,6 +1,7 @@
+drop table sales;
 create table if not exists sales
 (
-  date timestamp with time zone,
+  date date,
   sku_code varchar,
   quantity integer,
   price numeric,
@@ -8,100 +9,16 @@ create table if not exists sales
   constraint sales_pkey primary key (id)
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS index_sales_sku_code_date
-  ON sales
-  USING btree
-  (key COLLATE pg_catalog."default", date);
+truncate sales;
 
-TRUNCATE sales;
+insert into sales values('2019-01-01', 'A001', 10, 100);
+insert into sales values('2019-01-02', 'A001', 1, 100);
+insert into sales values('2019-01-03', 'A001', 3, 100);
+insert into sales values('2019-01-05', 'A001', 3, 100);
 
-insert into sales values('2018-10-01 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-01 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-02 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-02 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-03 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-03 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-04 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-04 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-05 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-05 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-06 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-06 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-07 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-07 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-08 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-08 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-09 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-09 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-10 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-10 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-11 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-11 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-12 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-12 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-13 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-13 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-14 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-14 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-15 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-15 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-16 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-16 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-17 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-17 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-18 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-18 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-19 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-19 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-20 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-20 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-21 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-21 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-22 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-22 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-23 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-23 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-24 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-24 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-25 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-25 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-26 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-26 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-27 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-27 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-28 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-28 12:00:00', 'A001', 4, 100.00);
-insert into sales values('2018-10-29 11:00:00', 'A001', 2, 100.00);
-insert into sales values('2018-10-29 12:00:00', 'A001', 4, 100.00);
+select date(day) as day from generate_series('2019-01-01', '2019-01-07', interval '1 day') as day;
 
--- we want to get data about daily sum of sales for the product and the running total up to current date
--- there is no guarantee that there always be sales for every day
--- so if we simply group by the date there might be "holes"
-
--- because of that we will start with generating all possible 1-day perdiods
--- with another postgresql gem generate_series
-
-select start, start + interval '1 day' as end
-from generate_series('2018-10-01', '2018-10-31', interval '1 day') as start;
-
--- CTE is a way to make your SQL queries more readable
-
-with intervals as (
-  select start, start + interval '1 day' as end
-  from generate_series('2018-10-01', '2018-10-31', interval '1 day') as start
+with days as (
+  select date(day) as day from generate_series('2019-01-01', '2019-01-07', interval '1 day') as day
 )
-select * from intervals;
-
--- now lets join the sales data
-with intervals as (
-  select start, start + interval '1 day' as end
-  from generate_series('2018-10-01', '2018-10-31', interval '1 day') as start
-)
-select
-  intervals.start, intervals.end, s.*
-from
-  intervals left join sales s on (
-    s.sku_code = 'A001'
-    and s.date >= intervals.start
-    and s.date < intervals.end
-);
+select days.day, coalesce(sales.quantity * sales.price, 0) as total from days left join sales on (days.day = sales.date);
